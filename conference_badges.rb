232c65f597961443097a877e485b9f4de79c35f1
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-14 04:27:26 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,6 @@ speakers_lineup = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(speakers_lineup)
  speakers_lineup.collect {|speaker| badge_maker(speaker)}
 end
+def assign_rooms(speakers_lineup)
+ speakers_lineup.each_with_index.map {|speaker, index| "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+end
